8c3c5a8f7f4a8cee4c04238c6a80b764bb245dbf
Patrick <patricktsaikl@gmail.com>
2024-06-09 14:00:26 +0800

    react internals deep dive: ch1 notes

--- react/react-internals-deep-dive/ch1/src/main.jsx
+++ react/react-internals-deep-dive/ch1/src/main.jsx
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom/client";
 
 function App() {
   const [count, setCount] = useState(1);
+  // II Render: go through the components and decide what to be updated on the DOM tree
   debugger;
 
   useEffect(() => {
+    // IV Trigger: Restart an update life cycle by setState (-> II -> III)
     debugger;
 
     setCount((count) => count + 1);
   }, []);
+  // III Commit: commit the update to the DOM tree
   return <button>{count}</button>;
 }
 
+// I Trigger: Entry point for the React components
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <App />
